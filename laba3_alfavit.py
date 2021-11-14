626fcc3e8c9e107206f6ea96d099337e4a15f29c
Dasha Gurska <90865882+Meczta@users.noreply.github.com>
2021-11-14 19:48:38 +0300

    Update laba3_alfavit.py

--- laba3_alfavit.py
+++ laba3_alfavit.py
@@ -1,12 +1,23 @@
 sort = str.lower(input('Введите строку:'))
+print('Выберите действие: \n 1:сортировать слова \n 2:показать количество букв')
+vibor = input(':::')
 splsort = sort.split()
 a = set(splsort)
-dr = str(a)
 e = ''
+d = {}
 for i in sorted(a):
-    e += ' ' + i
-print(e)
-d={}
-for char in set(dr):
-    d[char]=dr.count(char)
-print(d)
+    if len(i) > 3:
+        e += ' ' + i
+    else:
+        continue
+if vibor == '1':
+    print(e)
+elif vibor == '2':
+    for char in set(e):
+        if char != ' ':
+            d[char] = e.count(char)
+        else:
+            continue
+    print(d)
+else:
+    print('Вы ввели недопустимое значение')
